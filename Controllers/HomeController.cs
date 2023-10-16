b2d0203dbf8a1cc0a6e73bab540d5e9129d81322
liamc <liamc@LIAMSXPS15>
2023-10-16 11:57:14 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            ViewBag.Monkeys = Request.Form["nMonkeys"];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            return View(student);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab3.Models
+{
+    public class Student
+    {
+        public string FName
+        {
+            get;
+            set;
+        }
+
+        public string LName
+        {
+            get;
+            set;
+        }
+
+        public int StudentId
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
